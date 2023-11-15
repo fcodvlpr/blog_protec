837f789725aaec626cc25fe4505d6dcf344d3fd3
devfco <franciscodlx@outlook.es>
2023-11-14 23:00:06 -0600

    [B-0001] - Remove local image and add URL image, add variables sass

--- resources/js/site/pages/NewPost.jsx
+++ resources/js/site/pages/NewPost.jsx
@@ -105,7 +105,6 @@ const NewPost = () => {
                       data="<p>Hello from CKEditor&nbsp;5!</p>"
                       onReady={ editor => {
                           // You can store the "editor" and use when it is needed.
-                          // console.log( 'Editor is ready to use!', editor );
                           editor.editing.view.change((writer) => {
                             writer.setStyle(
                                 "height",
@@ -116,14 +115,6 @@ const NewPost = () => {
                       } }
                       onChange={ ( event, editor ) => {
                           const data = editor.getData();
-                          console.log({data})
-                          // console.log( { event, editor, data } );
-                      } }
-                      onBlur={ ( event, editor ) => {
-                          // console.log( 'Blur.', editor );
-                      } }
-                      onFocus={ ( event, editor ) => {
-                          // console.log( 'Focus.', editor );
                       } }
                   />
                   <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
